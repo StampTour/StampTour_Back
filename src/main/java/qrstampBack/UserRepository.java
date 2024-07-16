b54babd82ecc80feb49dd81a0dbea90c7eab9b51
sarangLim <115855158+dream-night@users.noreply.github.com>
2024-07-16 16:09:26 +0900

    api first test

--- src/main/java/qrstampBack/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package qrstampBack;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUserid(String userid);
-    void deleteSessionDataFromDatabase();
-}
