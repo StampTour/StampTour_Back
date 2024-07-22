9721218e3f9cd405ad8a0016440f055e338a870f
Dalliaaaaa <ajswlsoato@naver.com>
2024-07-22 11:23:55 +0900

    test: QrStamp의 login password 확인

--- src/test/java/com/example/stamp_demo/controller/StampControllerTest.java
+++ src/test/java/com/example/stamp_demo/controller/StampControllerTest.java
@@ -56,4 +56,19 @@ public class StampControllerTest {
         // Qr 엔티티를 찾지 못했으므로 404 Not Found 응답 확인
         assertEquals(ResponseEntity.notFound().build(), response);
     }
+
+    //패스워드가 QrStamp에서도 만들어졌는지 확인
+    @Test
+    public void testUpdateDataBadRequest() {
+        // 세션에서 패스워드 가져오기
+        when(session.getAttribute("password")).thenReturn("testPassword");
+        // QrService에서 해당 패스워드로 QR 엔티티 찾기
+        when(qrService.findByUsrpw("testPassword")).thenReturn(Optional.of(qrStamp));
+
+        // 잘못된 param 값 테스트
+        ResponseEntity<QrResponseDto> response = stampController.updateData(11, qrStamp, session);
+
+        // 400 Bad Request 응답 확인
+        assertEquals(ResponseEntity.badRequest().build(), response);
+    }
 }
